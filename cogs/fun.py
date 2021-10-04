8c5ec106971b005834af191f65422d0f33656ee0
KayTwenty <davidanton037@gmail.com>
2021-10-03 22:47:15 -0400

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -141,7 +141,7 @@ class Fun(commands.Cog):
         @commands.command(aliases=['sex', 'fuck']) #Frick Command
         async def frick(self, ctx, member: discord.Member):
             embed = discord.Embed(title="This person had sex with you ;)", description="**{1}** fucked **{0}**!".format(member.name, ctx.message.author.name), color=0x680af5)
-            embed.set_author(name="Fricked by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
+            embed.set_author(name="Fucked by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
             embed.set_image(url="https://media1.tenor.com/images/fa98b23ca1dba1925da62f834f27153f/tenor.gif?itemid=19355212")
             embed.set_footer(text="Command: 9frick @user")
             await ctx.reply(embed=embed)
