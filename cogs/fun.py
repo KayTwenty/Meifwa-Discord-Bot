fcf28d187f33d6a3736355ffdb259144265e35cd
KayTwenty <davidanton037@gmail.com>
2021-10-06 07:01:41 -0400

    Boot Update 1

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,20 +1,13 @@
 import discord, asyncio
-import rethinkdb as r
 import aiohttp
 import random
-import base64, json
 import logging
-import re
-import typing
 from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
-from .utils.chat_formatting import bold
 
 from .utils import helpers
 
-r_conn = r.connect(db="meifwa")
-
-class Fun(commands.Cog):
+class fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
@@ -22,121 +15,6 @@ class Fun(commands.Cog):
     def cog_unload(self):
         self.session.close()
         del self.session
-
-    async def get_cached_user(self, user_id: int):
-            cache = await self.bot.redis.get("user_cache:{}".format(user_id))
-            if cache is None:
-                cache = await self.bot.fetch_user(user_id)
-                cache = {
-                    "name": cache.name,
-                    "id": cache.id,
-                    "discriminator": cache.discriminator
-                }
-                await self.bot.redis.set("user_cache:{}".format(user_id), base64.b64encode(
-                    json.dumps(cache).encode("utf8")
-                ).decode("utf8"), expire=1800)
-            else:
-                cache = json.loads(base64.b64decode(cache))
-            return cache
-
-    @commands.command()
-    @commands.guild_only()
-    @commands.cooldown(1, 4, commands.BucketType.user)
-    async def marry(self, ctx, user: discord.Member):
-        if user == ctx.author:
-            return await ctx.send(bold("You can't marry yourself."))
-        author_data = r.table("marriage").get(str(ctx.author.id)).run(r_conn)
-        if not author_data:
-            author_data = {
-                "id": str(ctx.author.id),
-                "marriedTo": []
-            }
-        r.table("marriage").insert(author_data).run(r_conn)
-        
-        if str(user.id) in author_data.get("marriedTo", []):
-            return await ctx.send(bold("You are already married to that user."))
-        elif len(author_data.get("marriedTo", [])) >= 4:
-            return await ctx.send(bold("You are married to too many users"))
-
-        user_data = r.table("marriage").get(str(user.id)).run(r_conn)
-        if not user_data:
-            user_data = {
-                "id": str(user.id),
-                "marriedTo": []
-            }
-        r.table("marriage").insert(user_data).run(r_conn)
-        
-        if len(user_data.get("marriedTo", [])) >= 4:
-            return await ctx.send("That user is already married to too many users")
-
-        a_name = helpers.clean_text(ctx.author.name)
-        u_name = helpers.clean_text(user.name)
-        await ctx.send("{} is wanting to marry {}!\n{} type yes to accept!".format(a_name, u_name, user.mention))
-
-        try:
-            msg = await self.bot.wait_for("message", check=lambda x: x.channel == ctx.message.channel and x.author == user, timeout=15.0)
-            if msg.content.lower() != "yes":
-                return await ctx.send("Marriage Cancelled.")
-        except asyncio.TimeoutError:
-            return await ctx.send("Marriage Cancelled.")
-
-        await ctx.send(f"🎉 {ctx.author.mention} ❤ {user.mention} 🎉")
-
-        author_marriedTo = author_data.get("marriedTo", [])
-        user_marriedTo = user_data.get("marriedTo", [])
-        author_marriedTo.append(str(user.id))
-        user_marriedTo.append(str(ctx.author.id))
-        r.table("marriage").get(str(ctx.author.id)).update({"marriedTo": author_marriedTo}).run(r_conn)
-        r.table("marriage").get(str(user.id)).update({"marriedTo": user_marriedTo}).run(r_conn)
-
-    @commands.command()
-    @commands.guild_only()
-    @commands.cooldown(1, 4, commands.BucketType.user)
-    async def divorce(self, ctx, user: typing.Union[discord.User, discord.Member, int]):
-        if isinstance(user, int):
-            try:
-                converter = commands.UserConverter()
-                user = await converter.convert(ctx, str(user))
-            except commands.BadArgument:
-                user_re_match = re.match("[0-9]{12,22}", str(user))
-                if user_re_match is None:
-                    return await ctx.send_help(ctx.command)
-                user = await self.bot.fetch_user(int(user_re_match.group(0)))
-
-        if user.id == ctx.author.id:
-            return await ctx.send("You can't divorce yourself")
-
-        author_data = r.table("marriage").get(str(ctx.author.id)).run(r_conn)
-        if not author_data:
-            return await ctx.send(bold("You are not married"))
-        user_data = r.table("marriage").get(str(user.id)).run(r_conn)
-        if not user_data:
-            return await ctx.send("That user is not married to anyone")
-        if not str(ctx.author.id) in user_data.get("marriedTo", []):
-            return await ctx.send("That user is not married to you")
-
-        await ctx.send("**Are you sure you want to divorce {}?**".format(helpers.clean_text(user.name)))
-
-        try:
-            msg = await self.bot.wait_for("message", check=lambda x: x.channel == ctx.message.channel and x.author == ctx.author, timeout=15.0)
-            if msg.content.lower() != "yes":
-                return await ctx.send("**Cancelled.**")
-        except asyncio.TimeoutError:
-            return await ctx.send("**Cancelled.**")
-
-        new_author_married = []
-        for u in author_data.get("marriedTo", []):
-            if u != str(user.id):
-                new_author_married.append(u)
-
-        new_user_married = []
-        for u in user_data.get("marriedTo", []):
-            if u != str(ctx.author.id):
-                new_user_married.append(u)
-
-        r.table("marriage").get(str(user.id)).update({"marriedTo": new_user_married}).run(r_conn)
-        r.table("marriage").get(str(ctx.author.id)).update({"marriedTo": new_author_married}).run(r_conn)
-        await ctx.send("{} divorced {} 😦😢".format(helpers.clean_text(ctx.author.name), helpers.clean_text(user.name)))
 
     @commands.command(aliases=['sex', 'fuck']) #Frick Command
     async def frick(self, ctx, member: discord.Member):
@@ -157,5 +35,23 @@ class Fun(commands.Cog):
         embed.set_footer(text="Command: ;fuck @user")
         await ctx.reply(embed=embed)
 
+    @commands.command(name="invite", description="Get a invite link to add me to your server")
+    async def invite(self, ctx):
+        perms = discord.Permissions.all()
+
+        em = discord.Embed(color=discord.Color.blurple())
+
+        em.set_author(name=self.bot.user.name,
+                    icon_url=self.bot.user.avatar_url)
+        em.set_thumbnail(url=ctx.message.author.avatar_url)
+        em.add_field(
+            name="Invite Me!",
+            inline=False,
+            value=f"[Click Here](<{discord.utils.oauth_url(self.bot.user.id, permissions=perms)}>)",
+        )
+
+        em.set_footer(text=f"{ctx.author}", icon_url=ctx.message.author.avatar_url)
+        await ctx.send(embed=em)
+
 def setup(bot):
-    bot.add_cog(Fun(bot))
+    bot.add_cog(fun(bot))
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import os
 import json
 import asyncio
 import aiohttp
-import rethinkdb as r
 from asyncio import sleep
 from discord.ext import commands
 
@@ -23,9 +22,6 @@ BOT_PREFIX = configData["Prefix"]
 client = commands.Bot(command_prefix = BOT_PREFIX, case_insensitive=True)
 client.remove_command('help')
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
 
 async def status(): #Status changer for the bot
     while True:
@@ -35,59 +31,5 @@ async def status(): #Status changer for the bot
         await client.change_presence(status=discord.Status.online, activity=discord.Game(";invite"))
         await sleep(1500)
 
-async def _init_rethink():
-    r_conn = r.connect(host="127.0.0.1", port=28015, db="meifwa")
-
-@client.event
-async def on_ready():
-    print("Logged in as: " + client.user.name + "\n")
-client.loop.create_task(status())
-client.loop.create_task(_init_rethink())
-
-@client.command(name="invite", description="Get a invite link to add me to your server")
-async def invite(ctx):
-
-    perms = discord.Permissions.all()
-
-    em = discord.Embed(color=discord.Color.blurple())
-
-    em.set_author(name=client.user.name,
-                icon_url=client.user.avatar_url)
-    em.set_thumbnail(url=ctx.message.author.avatar_url)
-    em.add_field(
-        name="Invite Me!",
-        inline=False,
-        value=f"[Click Here](<{discord.utils.oauth_url(client.user.id, permissions=perms)}>)",
-    )
-    
-    em.set_footer(text=f"{ctx.author}", icon_url=ctx.message.author.avatar_url)
-    await ctx.send(embed=em)
-
-async def send_cmd_help(ctx):
-    return await ctx.send_help(ctx.command)
-
-async def on_command_error(ctx, exception):
-    error = getattr(exception, "original", exception)
-    if isinstance(error, discord.NotFound):
-        return
-    elif isinstance(error, discord.Forbidden):
-        return
-    elif isinstance(error, discord.HTTPException) or isinstance(error, aiohttp.ClientConnectionError):
-        return await ctx.send("Failed to get data.")
-    if isinstance(exception, commands.NoPrivateMessage):
-        return
-    elif isinstance(exception, commands.DisabledCommand):
-        return
-    elif isinstance(exception, commands.BadArgument):
-        await client.send_cmd_help(ctx)
-    elif isinstance(exception, commands.MissingRequiredArgument):
-        await client.send_cmd_help(ctx)
-    elif isinstance(exception, commands.CheckFailure):
-        await ctx.send("You are not allowed to use that command.", delete_after=5)
-    elif isinstance(exception, commands.CommandOnCooldown):
-        await ctx.send("Command is on cooldown... {:.2f}s left".format(exception.retry_after), delete_after=5)
-    elif isinstance(exception, commands.CommandNotFound):
-        return
-    return
 
 client.run(TOKEN)
