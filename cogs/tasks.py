03398278698fc541028afba99a04898cd4523420
KayTwenty <davidanton037@gmail.com>
2021-12-02 10:39:44 -0500

    Finishing Cleaning

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -2,7 +2,6 @@ from asyncio import sleep
 
 from discord.ext import commands, tasks
 import discord
-
 from boot.meifwa import MeifwaBot
 
 
@@ -14,13 +13,13 @@ class Tasks(commands.Cog):
     @tasks.loop()
     async def status_handler(self):
         await self.bot.wait_until_ready()
+        await self.bot.change_presence(activity=discord.Game("PADORU PADORU!!"))
+        await sleep(6000)
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"@{self.bot.user.name} help"))
         await sleep(800)
         await self.bot.change_presence(activity=discord.Game(f"with {len(self.bot.users)} humans!"))
         await sleep(800)
         await self.bot.change_presence(activity=discord.Game(f"in {len(self.bot.guilds)} servers"))
-        await sleep(800)
-        await self.bot.change_presence(activity=discord.Game("PADORU PADORU"))
         await sleep(800)
         await self.bot.change_presence(activity=discord.Game("Onii Chan"))
         await sleep(800)
