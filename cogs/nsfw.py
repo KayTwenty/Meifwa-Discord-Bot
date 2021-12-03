4bdbc8f908ca48e133039f1dae8104a43f625f0f
KayTwenty <davidanton037@gmail.com>
2021-12-02 19:51:43 -0500

    Uh oh

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -1,6 +1,7 @@
 import discord, asyncio
 import aiohttp
 import logging
+import random
 
 from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
@@ -471,6 +472,49 @@ class Nsfw(commands.Cog):
             )
             await ctx.message.reply(embed=embed, delete_after=20) 
 
+
+    @commands.command(aliases=["hn"])
+    @commands.cooldown(1, 20, commands.BucketType.user)
+    async def hentainuke(self, ctx: commands.Context, *, tag: str = None):
+        available_tags = ["waifu", "neko", "trap", "blowjob"]
+
+        if tag is None:
+            tag = random.choice(available_tags)
+
+        if tag is not None and tag.lower() == "list":
+            tags = "\n".join(available_tags)
+            return await ctx.send(
+                embed=discord.Embed(
+                    title="Available Tags",
+                    description=tags,
+                    color=self.bot.ok_color,
+                )
+            )
+
+        if tag is not None and tag.lower() in available_tags:
+            async with self.bot.session.post(
+                url=f"https://api.waifu.pics/many/nsfw/{tag}",
+                headers={
+                    "Accept": "application/json",
+                    "content-type": "application/json",
+                },
+                json={"files": ""},
+            ) as resp:
+                step = 5  # the amount of files to display at a time
+                idx = 5  # set the index to start with
+                files = (await resp.json())["files"]
+                while idx < len(files):
+                    sublist = files[idx - step : idx]  # [0:5], [5:10], etc
+                    await ctx.send("\n".join(map(str, sublist)))
+                    idx += step
+        else:
+            await ctx.send(
+                embed=discord.Embed(
+                    title="TAG NOT FOUND",
+                    description=f"{tag} was not found in the available tag list. Please run `{ctx.prefix}hb list`",
+                    color=self.bot.error_color,
+                )
+            )
 
 def setup(bot):
     bot.add_cog(Nsfw(bot))
