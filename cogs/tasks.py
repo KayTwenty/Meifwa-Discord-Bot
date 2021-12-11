312b62d30bc1ea01b781baa2d771b14f0c13a18b
KayTwenty <davidanton037@gmail.com>
2021-12-11 09:24:48 -0500

    Update tasks.py

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -13,15 +13,15 @@ class Tasks(commands.Cog):
     @tasks.loop()
     async def status_handler(self):
         await self.bot.wait_until_ready()
-        await self.bot.change_presence(activity=discord.Game("PADORU PADORU!!"))
-        await sleep(100)
+        await self.bot.change_presence(activity=discord.Game("Merry Christmas!! 🎄"))
+        await sleep(300)
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"@{self.bot.user.name} help"))
         await sleep(100)
         await self.bot.change_presence(activity=discord.Game(f"with {len(self.bot.users)} humans!"))
         await sleep(100)
         await self.bot.change_presence(activity=discord.Game(f"in {len(self.bot.guilds)} servers"))
         await sleep(100)
-        await self.bot.change_presence(activity=discord.Game("Onii Chan"))
+        await self.bot.change_presence(activity=discord.Game("PADORU PADORU!!"))
         await sleep(100)
         
 def setup(bot):
